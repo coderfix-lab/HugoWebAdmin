626fa7b13bcc6dbcede5bcf1851c5816839f02fb
xiaoyu <316708138@qq.com>
2020-02-13 00:55:22 +0800

    清理项目

--- config/web.php
+++ config/web.php
@@ -117,78 +117,7 @@ $config = [
                 ],
             ],
         ],
-        'core' => [
-            'class' => 'app\modules\core\Module',
-        ],
-        'agent' => [
-            'class' => 'app\modules\agent\Module',
-        ],
-        'goods' => [
-            'class' => 'app\modules\goods\Module',
-        ],
-        'trade' => [
-            'class' => 'app\modules\trade\Module',
-        ],
-        'content' => [
-            'class' => 'app\modules\content\Module',
-        ],
-        'online' => [ //线上商城
-            'class' => 'app\modules\online\Module',
-        ],
-        //仓库管理
-        'store' => [
-            'class' => 'app\modules\store\Module',
-        ],
 
-//        //运维平台接口
-//        'APIsys' => [
-//            'class' => 'app\modulesAPI\sys\Module',
-//        ],
-
-        //内部工具
-        'tool' => [
-            'class' => 'app\modules\tool\Module',
-        ],
-
-        //发布订阅
-        'topic' => [
-            'class' => 'app\modules\topic\Module',
-        ],
-
-        //营销模块
-        'marketing' => [
-            'class' => 'app\modules\marketing\Module',
-        ],
-
-        //数据相关
-        'purviewz' => [
-            'class' => 'app\modules\purviewz\Module',
-        ],
-        'tradez' => [
-            'class' => 'app\modules\tradez\Module',
-        ],
-        'agentz' => [
-            'class' => 'app\modules\agentz\Module',
-        ],
-        'corez' => [
-            'class' => 'app\modules\corez\Module',
-        ],
-        //系统配置 业务方向
-        'system' => [
-            'class' => 'app\modules\system\Module',
-        ],
-        //二手模块
-        'past' => [
-            'class' => 'app\modules\past\Module',
-        ],
-        //寄售入库
-        'consign' => [
-            'class' => 'app\modules\consign\Module',
-        ],
-        //寄售商品库
-        'dict' => [
-            'class' => 'app\modules\dict\Module',
-        ],
 
     ],
     'params' => $params,
